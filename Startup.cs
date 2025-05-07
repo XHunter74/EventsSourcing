5a35a76e7729fd44c84ae66d912df9d71621904b
serhiy.krasovskyy <xhunter74@gmail.com>
2025-05-07 14:56:54 +0300

    Initial commit.

---
+++ Startup.cs
@@ -0,0 +1,39 @@
+﻿using CQRSMediatr;
+
+namespace EventSourcing;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+
+        services.AddCqrsMediatr(typeof(Startup));
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder builder, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            builder.UseDeveloperExceptionPage();
+        }
+
+        //Allow all CORS
+        builder.UseCors("CorsPolicy");
+
+        builder.UseRouting();
+
+        builder.UseAuthentication();
+        builder.UseAuthorization();
+
+        builder.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+    }
+}
