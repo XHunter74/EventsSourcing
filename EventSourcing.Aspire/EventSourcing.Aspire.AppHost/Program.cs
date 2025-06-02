9e824e9cd2acafec58264b27abcd59903979b570
serhiy.krasovskyy <xhunter74@gmail.com>
2025-06-02 10:27:52 +0300

    Reorganized. Added Aspire project.

---
+++ EventSourcing.Aspire/EventSourcing.Aspire.AppHost/Program.cs
@@ -0,0 +1,17 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var username = builder.AddParameter("username", secret: true);
+var password = builder.AddParameter("password", secret: true);
+
+var postgres = builder.AddPostgres("postgres", username, password)
+    .WithImage("postgres:13-alpine")
+    .WithPgAdmin();
+var postgresdb = postgres.AddDatabase("event-sourcing");
+
+
+// Register the API project and connect it to the Postgres database
+builder.AddProject<Projects.EventSourcing>("api")
+    .WithReference(postgresdb, "DbConnection")
+    .WaitFor(postgresdb);
+
+builder.Build().Run();
